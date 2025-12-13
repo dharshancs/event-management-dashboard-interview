9139b78c2082c751af49c6dc2e8bee6aa1eb2cc5
23f3004028@WIN11 <23f3004028@ds.study.iitm.ac.in>
2025-12-13 16:42:19 +0530

    Routes created for login and register. Templates for dahboard yet to be created

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 contains 3 tables {users,events,registrations}
 event manager will be auto registered(creds = admin,admin)
+add reference to frontend files to mad-1 project
--- controllers/base_view.py
+++ controllers/base_view.py
@@ -1,13 +1,62 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,session,redirect,url_for,flash
 from .__init__ import connect_database
 
 u_view = Blueprint('base',__name__)
 
 
-@u_view.route('/')
+@u_view.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template("index.html")
+    if request.method == 'POST':
+        email = request.form['user_email']
+        password = request.form['user_password']
+        conn = connect_database()
+        curr = conn.cursor()
+        curr.execute('SELECT * FROM USERS WHERE email=?',(email,))
+        user=curr.fetchone()
+        conn.close()
+        if user and password==user['password']:
+            if not user['is_admin']:
+                session['id'] = user['id']
+                session['email'] = user['email']
+                session['is_admin'] = False
+                return "USER DASHBOARD" # to be added
+            elif user['is_admin']:
+                session['id'] = user['id']
+                session['email'] = user['email']
+                session['is_admin'] = True
+                return "ADMIN DASHBOARD" # to be added
+        else:
+            flash("Incorrect Email or Password","danger")
+    return render_template('index.html')
 
-@u_view.route('/register')
+
+@u_view.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    if request.method == 'POST':
+        name = request.form['user_name']
+        email = request.form['user_email']
+        password = request.form['user_password']
+        confirm_password = request.form['user_confirm_password']
+        
+        conn = connect_database()
+        curr = conn.cursor()
+
+        if not(password == confirm_password):
+            flash("Password do not match","danger")
+            conn.close()
+            return redirect(url_for('base.register'))
+        
+        curr.execute('SELECT id from users WHERE email = ?',(email,))
+        existing_user = curr.fetchone()
+
+        if existing_user:
+            flash("Email already registered!. Login or use a different email", "danger")
+            conn.close()
+            return redirect(url_for('base.home'))
+        
+        curr.execute('INSERT INTO USERS (name,email,password) VALUES (?,?,?)',(name,email,password))
+        conn.commit()
+        conn.close()
+        flash("Account Registered Succesfully","success")
+        return redirect(url_for('base.home'))
+    return render_template('register.html')
