7ee3af2da3d9523342ad234057f39ddff25ecca1
23f3004028@WIN11 <23f3004028@ds.study.iitm.ac.in>
2025-12-13 15:36:48 +0530

    Database Schema Created

---
+++ models/database.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+def create_database():
+    conn = sqlite3.connect("Event_management.db")
+    conn.execute("PRAGMA foreign_keys =ON")
+
+    cur=conn.cursor()
+
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS USERS(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                email TEXT NOT NULL UNIQUE,
+                password TEXT NOT NULL,
+                is_admin BOOLEAN DEFAULT FALSE
+                );
+                ''')
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS EVENTS(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                description TEXT NOT NULL,
+                date DATETIME,
+                created_by INTEGER
+                FOREIGN KEY(created_by) REFERENCES USERS(id)
+                );
+                ''')
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS REGISTRATIONS(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER NOT NULL,
+                event_id NTEGER NOT NULL,
+                FOREIGN KEY(user_id) REFERENCES USERS(id)
+                FOREIGN KEY(event_id) REFERENCES EVENTS(id)
+                );
+                ''')
+    cur.commit()
+    cur.close()
+    
