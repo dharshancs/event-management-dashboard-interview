a5a33505d19586c246134299ffad7092721a6390
23f3004028@WIN11 <23f3004028@ds.study.iitm.ac.in>
2025-12-13 16:26:02 +0530

    Base frontend templates created for login and register

--- controllers/base_view.py
+++ controllers/base_view.py
@@ -7,3 +7,7 @@ u_view = Blueprint('base',__name__)
 @u_view.route('/')
 def home():
     return render_template("index.html")
+
+@u_view.route('/register')
+def register():
+    return render_template("register.html")
